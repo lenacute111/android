fbda206903ffc70fac98687c5998c0164b761f32
lenacute111 <natl.21da@vku.udn.vn>
2023-09-21 23:40:03 +0700

    add lap 2

---
+++ DiceRoller2/app/src/main/java/com/na/diceroller2/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.na.diceroller2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            val resultTextView: TextView = findViewById(R.id.textView4)
+            resultTextView.text = (1..6).random().toString()
+        }
+    }
+}
